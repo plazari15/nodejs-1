6d249e0081c2d1838f4ef5c2123927b8f21c2578
Pedro <pedro.lazari@fcamara.com.br>
2019-08-20 00:09:26 -0300

    Criando primeiro retorno com query

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const express = require('express');
 
 const server = express();
+
+server.get('/teste', (request, response) => {
+    console.log('Inciando rota teste');
+    const { retorno } = request.query;
+
+    console.log(retorno);
+    
+    
+    if(retorno == 'json'){
+        console.log('Aqui vamos retornar um Json');
+        return response.send({ message : 'Hello World'});
+    }
+    return response.send('Hello World');
+})
+
+server.listen(3000);
