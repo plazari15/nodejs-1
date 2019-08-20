5bd87ef90ac38296b8e581d3a23108032600f2e4
Pedro <pedro.lazari@fcamara.com.br>
2019-08-20 00:40:30 -0300

    Criando CRUD na aplicação

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json()); //Express lerá preferencialmente JSON
+
+/**
+ * VETOR de usuários
+ */
+const users = ['Pedro', 'Undersvaldo', "Marconato"]
 /**
  * Rita teste apeanas com parametro para saber se o retorno será
  * em json ou não
@@ -18,7 +24,7 @@ server.get('/teste', (request, response) => {
         return response.send({ message : 'Hello World'});
     }
     return response.send('Hello World');
-})
+});
 
 /**
  * Rota retorna o nome do usuário também com parametros de query
@@ -29,7 +35,7 @@ server.get('/returnMyName', (request, response) => {
     return response.send({
         name: `Seu nome é ${nome}`
     });
-})
+});
 
 /**
  * Rota retorna usuário id pelos parametros de rota
@@ -37,7 +43,38 @@ server.get('/returnMyName', (request, response) => {
 server.get('/users/:id', (request, response) => {
     const id = request.params.id;
 
-     return response.json({user:"Buscando usuário "+id });
+     return response.json({user:users[id]});
+});
+
+
+/**
+ * Rota retorna usuário id pelos parametros de rota
+ */
+server.put('/users/:id', (request, response) => {
+    const id = request.params.id;
+    const { name } = request.body;
+
+    users[id] = name;
+
+    return response.json({users});
+});
+
+
+server.post('/users', (request, response) => {
+    const {name} = request.body;
+
+    users.push(name);
+
+    return response.json({users});
+});
+
+server.delete('/users/:id', (request, response) => {
+    const { id } = request.params;
+
+    users.splice(id, 1);
+
+    return response.send(); 
 })
+
 
 server.listen(3000);
