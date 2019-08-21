8b16d6c4a1436aefafd3ec908b63f334539f4b12
Pedro <pedro.lazari@fcamara.com.br>
2019-08-20 22:51:25 -0300

    Aplicando Middlewares

--- index.js
+++ index.js
@@ -8,11 +8,52 @@ server.use(express.json()); //Express lerá preferencialmente JSON
  * VETOR de usuários
  */
 const users = ['Pedro', 'Undersvaldo', "Marconato"]
+
+/**
+ * Criando um Middleware (GLOBAL) para manipular todas as requisições
+ *
+ * Middleware é um interceptador
+ */
+server.use((request, response, next) => {
+    console.time('Request');
+    console.log(`Método: ${request.method}; URL: ${request.url}`);
+
+    next();
+
+    console.timeEnd('Request');
+});
+
+/**
+ * Criando agora um Middleware Local
+ */
+function checkUserExists(request, response, next) {
+    if(!request.body.name){
+        return response.status(400).json({ error : "User not found on request body"});
+    }
+
+    return next();
+}
+
+/**
+ * Criando um Middleware apenas para verificar se o usuário existe
+ */
+function checkUserInArray(request, response, next) {
+    const user = users[request.params.id];
+    if(!user){
+        return response.status(400).json({error: "User does not exists"});
+    }
+
+    //Middleware pode alterar req e response (principalmente req)
+
+    request.user = user;
+
+    return next();
+}
 /**
  * Rita teste apeanas com parametro para saber se o retorno será
  * em json ou não
  */
-server.get('/teste', (request, response) => {
+server.get('/teste',  (request, response) => {
     console.log('Inciando rota teste');
     const { retorno } = request.query;
 
@@ -40,17 +81,17 @@ server.get('/returnMyName', (request, response) => {
 /**
  * Rota retorna usuário id pelos parametros de rota
  */
-server.get('/users/:id', (request, response) => {
-    const id = request.params.id;
+server.get('/users/:id', checkUserInArray, (request, response) => {
+    //const id = request.params.id;
 
-     return response.json({user:users[id]});
+     return response.json({user:request.user});
 });
 
 
 /**
  * Rota retorna usuário id pelos parametros de rota
  */
-server.put('/users/:id', (request, response) => {
+server.put('/users/:id', checkUserExists, checkUserInArray,  (request, response) => {
     const id = request.params.id;
     const { name } = request.body;
 
@@ -60,7 +101,7 @@ server.put('/users/:id', (request, response) => {
 });
 
 
-server.post('/users', (request, response) => {
+server.post('/users', checkUserExists, (request, response) => {
     const {name} = request.body;
 
     users.push(name);
@@ -68,7 +109,7 @@ server.post('/users', (request, response) => {
     return response.json({users});
 });
 
-server.delete('/users/:id', (request, response) => {
+server.delete('/users/:id', checkUserInArray, (request, response) => {
     const { id } = request.params;
 
     users.splice(id, 1);
